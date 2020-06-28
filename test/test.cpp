aa3f0af45cc7da0b1779fce1dc8d47c63cdd7215
Przemysław Papla <przemekpapla@gmail.com>
2020-06-28 10:00:33 +0200

    Reorganization of the project structure

--- test/test.cpp
+++ test/test.cpp
@@ -3,12 +3,12 @@
 #define BOOST_TEST_DYN_LINK
 
 #include <boost/test/unit_test.hpp>
-
-//...
+#include <math/vector.h>
 
 BOOST_AUTO_TEST_SUITE(my_testsuite)
 
 BOOST_AUTO_TEST_CASE(case_one) {
+    Vector v(0);
     BOOST_CHECK_EQUAL(0, 0);
 }
 
