24e884d20b33bc7950c8c05529afc58ddf600f9a
Przemysław Papla <przemekpapla@gmail.com>
2020-06-29 14:26:48 +0200

    TODO: Link GLEW

--- src/engine/core/window.cpp
+++ src/engine/core/window.cpp
@@ -1,3 +1,40 @@
 #include "window.h"
 
-//...
+Window::Window(int width, int height, const char* title) {
+
+    if (!glfwInit()) {
+        throw InitException();
+    }
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    window = glfwCreateWindow(width, height, title, NULL, NULL);
+
+    if (!window) {
+        glfwTerminate();
+        throw  InitException();
+    }
+
+    glfwMakeContextCurrent(window);
+
+    glewExperimental = GL_TRUE;
+    glewInit();
+    glGetError();
+
+    glViewport(0, 0, width, height);
+
+    glEnable(GL_DEPTH_TEST);
+}
+
+Window::~Window() {
+    glfwTerminate();
+}
+
+int Window::shouldClose() {
+    return glfwWindowShouldClose(window);
+}
+
+void Window::swapBuffers() {
+    glfwSwapBuffers(window);
+}
--- src/engine/core/window.h
+++ src/engine/core/window.h
@@ -1,3 +1,20 @@
+#ifndef WINDOW_H
+#define WINDOW_H
+
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+#include "exception/InitException.h"
+
 class Window {
-    //...
+private:
+    GLFWwindow* window;
+public:
+    Window(int width, int height, const char* title);
+    ~Window();
+
+    int shouldClose();
+    void swapBuffers();
 };
+
+#endif // WINDOW_H
