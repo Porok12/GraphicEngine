88e52ebe6f789e0934bce6c0e508ece48cfbbadd
Przemysław Papla <przemekpapla@gmail.com>
2020-07-14 19:36:38 +0200

    Copy LightStage

---
+++ src/engine/gui/ShadingStage.h
@@ -0,0 +1,42 @@
+#ifndef SHADINGSTAGE_H
+#define SHADINGSTAGE_H
+
+#include <memory>
+#include <core/models/ModelRenderer.h>
+#include <core/particles/ParticleGenerator.h>
+#include <core/particles/ParticleRenderer.h>
+#include <core/models/Model.h>
+#include "UIStage.h"
+#include "UIFrameDecorator.h"
+#include "UIButton.h"
+
+class ShadingStage : public UIStage {
+private:
+    enum Shading {
+        FLAT,
+        GOURAUD,
+        PHONG,
+        BLINN
+    };
+
+    float a = 0;
+    Model model, plane;
+    std::shared_ptr<ShaderProgram> program, phong, gouraud;
+    std::shared_ptr<UIComponent> temp;
+
+    static std::shared_ptr<ShadingStage> instance;
+    ShadingStage();
+    void updateShading(Shading shading);
+
+public:
+    static const std::shared_ptr<ShadingStage> &getInstance();
+
+    void setXxx(std::function<void ()> fun) {
+        std::dynamic_pointer_cast<UIButton>(temp)->addClickCallback(fun);
+    }
+
+    void renderUI() override;
+    void renderContent(Camera camera, double dt) override;
+};
+
+#endif // SHADINGSTAGE_H
