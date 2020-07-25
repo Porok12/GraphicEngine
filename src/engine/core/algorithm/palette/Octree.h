7d93a4c9c83d193a46b596fa6db2603a321e6c35
Przemysław Papla <przemekpapla@gmail.com>
2020-07-25 16:03:29 +0200

    Add algorithms

---
+++ src/engine/core/algorithm/palette/Octree.h
@@ -0,0 +1,24 @@
+#ifndef OCTREE_H
+#define OCTREE_H
+
+#include <math/Vec3.h>
+
+#define OCTREE_DEPTH 8
+
+struct octreenode {
+    unsigned long references = 0;
+    unsigned long red = 0;
+    unsigned long green = 0;
+    unsigned long blue = 0;
+    octreenode *childs = nullptr;
+};
+
+octreenode* createOctree(const std::vector<fVec3> &colors);
+//void addNode(const octreenode &node, const fVec3 &color);
+
+class OctreePalete {
+public:
+    static void getPalette(int s, std::vector<fVec3> &colors, std::vector<fVec3> &palette);
+};
+
+#endif // OCTREE_H
